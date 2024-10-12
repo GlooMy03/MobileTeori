1b66e629ff59351f2540603fed5368a94cde6082
ALIMASHUDA <blu70125@gmail.com>
2024-10-12 10:49:19 +0700

    ali

--- lib/app/modules/home/bindings/coffe_powder_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../controllers/home_controller.dart'; // Or any other controller needed
-
-class CoffeePowderBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
-  }
-}
--- lib/app/modules/home/controllers/stock_coffe_powder_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class StockCoffePowderController extends GetxController {
-  // Define your state variables and methods here
-}
